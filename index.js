a07351fb4f30e29de405c03ef9d1f8ee330b2826
DaniellaHenh <daniellahamo23@gmail.com>
2025-08-27 16:39:39 +0300

    add Postgres connection

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿require('dotenv').config();
+const { Client } = require('pg');
+
+(async () => {
+  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  try {
+    await client.connect();
+    const { rows } = await client.query('select version(), current_database(), current_user;');
+    console.log(' Connected!', rows[0]);
+  } catch (err) {
+    console.error(' DB connection failed:', err);
+    process.exit(1);
+  } finally {
+    await client.end();
+  }
+})();
